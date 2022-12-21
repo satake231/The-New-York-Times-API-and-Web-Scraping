2c367bfb0dac0ae6d6bb2da2c1f9f321270d7a32
satake231 <r1ryoichiros2@gmail.com>
2022-12-21 15:15:31 +0900

    modify function

--- Function.py
+++ Function.py
@@ -9,17 +9,6 @@ from tqdm import tqdm
 import time
 
 def get_articles(start_year, end_year, your_api_key):
-    df_base = pd.DataFrame({'abstract':[0],
-                            'lead_paragraph':[0],
-                            'pub_date':[0],
-                            'print_headline':[0],
-                            'main_headline':[0],
-                            'keywords':[0],
-                            'news_desk':[0],
-                            'word_count':[0],
-                            'snippet':[0],
-                            'web_url':[0]}, index=[0])
-
     url = "https://api.nytimes.com/svc/archive/v1/{}/{}.json?api-key={}"
     path = "./{}_{}.csv"
 
@@ -31,7 +20,16 @@ def get_articles(start_year, end_year, your_api_key):
 
             res_json = res.json()
 
-            df = df_base
+            df = pd.DataFrame({'abstract':[0],
+                               'lead_paragraph':[0],
+                               'pub_date':[0],
+                               'print_headline':[0],
+                               'main_headline':[0],
+                               'keywords':[0],
+                               'news_desk':[0],
+                               'word_count':[0],
+                               'snippet':[0],
+                               'web_url':[0]}, index=[0])
 
             for i in tqdm(range(0, len(res_json['response']['docs']))):
                 tmp = []
@@ -41,9 +39,12 @@ def get_articles(start_year, end_year, your_api_key):
                 df.loc[i] = [res_json['response']['docs'][i]['abstract'], res_json['response']['docs'][i]['lead_paragraph'],
                              res_json['response']['docs'][i]['pub_date'], res_json['response']['docs'][i]['headline']['print_headline'],
                              res_json['response']['docs'][i]['headline']['main'], tmp, res_json['response']['docs'][i]['news_desk'],
-                        -     res_json['response']['docs'][i]['word_count'], res_json['response']['docs'][i]['snippet'],
+                             res_json['response']['docs'][i]['word_count'], res_json['response']['docs'][i]['snippet'],
                              res_json['response']['docs'][i]['web_url']]
 
             df.to_csv(path.format(year, month + 1))
 
-            time.sleep(1)
+            time.sleep(0)
+#%%
+
+#%%
